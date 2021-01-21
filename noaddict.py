63e4abe451294216ac5512cdeec6ece16e7000c7
ZombieNeo <46230458+ZombieNeo@users.noreply.github.com>
2021-01-21 14:39:24 +0000

    patch v1

--- noaddict.py
+++ noaddict.py
@@ -5,37 +5,62 @@ now = datetime.now()
 
 
 #functions
-def loop():
-    if start == "y":
-        print("you started! "+str(now))
-        save = open("save.txt","a")
-        save.write(str(now))
-        save.close()
+
+
+
+def retry():
+    print("start, current streak or end streak ")
+    choice = input("state ya purpose ")
+    choice.lower()
+    if choice == "start":
+        start()
         pass
-    else:
-        while True:
-            print("ok puss")
+    
+    if choice == "current":
+        current()
+    
+    if choice == "end":
+        end()    
+
+
+
+
+def loop():
+    save = open("save.txt","a")
+    save.write(str(now))
+    save.close()
+    save = open("save.txt","r")
+    file_contents = save.read()
+    print("mark the date: "+str(file_contents))
+    pass
 
     
 
 def start():
-    start = input("start? ")
-    start.lower()
+    print("GOOD LUCK SOLIDER! TriHard 7 ")
     loop()
     
     
 def current():
     f = open("save.txt","r")
-    file_contents =int(f.read())
+    file_contents = f.read()
     print("you started on: ")
-    print (int(file_contents))
-    now1 =int(datetime.now())
+    print(file_contents)
     f.close()
     
 def end():
     print("it's ok we all have bad days ")
+    f = open("save.txt","r")
+    file_contents = f.read()
+    print("you started on: ")
+    print(file_contents)
+    f.close()
+    print("you lost! BETTER LUCK NEXT TIME! ")
+    f = open("save.txt","w")
+    f.write("LLLLLLL")
     print("WIPING STREAK!")
-    start()
+    retry()
+    
     
 
 #functions
